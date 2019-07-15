ff4b1967a57d3e1d4b435ef056adc59cb233238e
Eliot Pickhardt <eliotpickhar@gmail.com>
2019-07-15 16:05:16 +0000

    ensuring progress was saved

--- dagmc_stats.py
+++ dagmc_stats.py
@@ -3,15 +3,14 @@ import sys
 sys.path.append('/opt/tljh/user/lib/moab/lib/python3.6/site-packages/pymoab-5.1.0-py3.6-linux-x86_64.egg')
 from pymoab import core, types
 from pymoab.rng import Range
-
+import numpy as np
+import matplotlib as plt
 def get_dagmc_tags(my_core):
     """
     Get a dictionary with the important tags for DAGMC geometries
-
     inputs
     ------
     my_core : a MOAB Core instance
-
     outputs
     -------
     dagmc_tags : a dictionary of relevant tags
@@ -30,16 +29,14 @@ def get_dagmc_tags(my_core):
     return dagmc_tags
 
 
-def get_entity_ranges(my_core, meshset, entity_types):
+def get_entity_ranges(my_core, meshset, entity_types, dagmc_tags):
     """
     Get a dictionary with MOAB ranges for each of the requested entity types
-
     inputs
     ------
     my_core : a MOAB Core instance
     meshset : a MOAB meshset to query for the ranges of entities
     entity_types : a list of valid pyMOAB types to be retrieved
-
     outputs
     -------
     entity_ranges : a dictionary with one entry for each entity type that is a Range of handles to that type
@@ -48,6 +45,152 @@ def get_entity_ranges(my_core, meshset, entity_types):
     entity_ranges = {}
     for entity_type in entity_types:
         entity_ranges[entity_type] = my_core.get_entities_by_type(meshset, entity_type) 
-
+        if entity_type == 11:
+            entity_ranges['Volumes'] = my_core.get_entities_by_type_and_tag(meshset, types.MBENTITYSET, dagmc_tags['geom_dim'], [3])
+            entity_ranges['Surfaces'] = my_core.get_entities_by_type_and_tag(meshset, types.MBENTITYSET, dagmc_tags['geom_dim'], [2])
+            entity_ranges['Curves'] = my_core.get_entities_by_type_and_tag(meshset, types.MBENTITYSET, dagmc_tags['geom_dim'], [1])
     return entity_ranges
 
+def get_surfaces_per_volume(my_core, entity_ranges):
+    """
+    Get the number of surfaces that each volume in a given file contains
+    
+    inputs
+    ------
+    my_core : a MOAB core instance
+    entity_ranges : a dictionary of the ranges of each tag in a file
+    
+    outputs
+    -------
+    pdf : a histogram that displays the number of surfaces each volume has (matplotlib)
+    """
+    freqs = []
+    for volumeset in entity_ranges['Volumes']:
+        freqs.append(my_core.get_child_meshsets(volumeset).size())
+    stats = {}
+    stats['minimum'] = min(freqs)
+    stats['maximum'] = max(freqs)
+    stats['median'] = np.median(freqs)
+    stats['average'] = np.average(freqs)
+    return stats
+
+def make_verts_dictionary(vert_list):
+    """
+    returns a dictionary of frequency of the values of the triangles per vertex (ie {number of triangles per vertex: occurences})
+    inputs
+    -----
+    vert_list : a list of all vertices in a file
+    
+    outputs:
+    --------
+    sorted_dict: a dictionary that has information about triangles per vertex, sorted by its keys.
+    """
+    vertices_and_frequencies = {} 
+    #makes dictionary with each vertex and the number of triangles on it
+    for vert in vert_list:
+        number_of_vertex = vert
+        try:
+            vertices_and_frequencies[number_of_vertex] += 1
+        except KeyError:
+            vertices_and_frequencies[number_of_vertex] = 1
+    number_of_frequencies = {}
+    #makes dictionary with the number of triangles, and the number of vertices that have that number
+    for vertex in vertices_and_frequencies:
+        try:
+            number_of_frequencies[vertices_and_frequencies[vertex]] += 1
+        except KeyError:
+            number_of_frequencies[vertices_and_frequencies[vertex]] = 1
+    sorted_dict = {}
+    #sorts the dictionary
+    for frequency in sorted(number_of_frequencies.keys()):
+        sorted_dict[frequency] = number_of_frequencies[frequency]
+    return(sorted_dict)
+def find_median(sorted_dict):
+    """
+    finds the median of a dictionary with format {value:occurences}
+    
+    inputs
+    ------
+    sorted_dict : a dictionary sorted by its keys
+    
+    outputs
+    -------
+    mean : the median of the dataset
+    
+    example dictionary:
+    {3:2, 4:5, 6:3} = [3,3,4,4,4,4,4,6,6,6]
+    """
+    #finds the number of data points
+    length = 0
+    for value in sorted_dict.values():
+        length += value
+    half = length / 2
+    sum_var = 0
+    #finds the index of the middle of the dataset
+    for val in sorted_dict.values():
+        if half-sum_var > 0:
+            sum_var += val
+        else:
+            break
+    index = (list(sorted_dict.values()).index(val))
+    #returns the median based off some characteristics of the dataset
+    if sum(list(sorted_dict.values())[index:]) != sum(list(sorted_dict.values())[:index]):
+        if sum(list(sorted_dict.values())[index:]) > sum(list(sorted_dict.values())[:index]):
+            median = list(sorted_dict.keys())[index]
+        else:
+            median = list(sorted_dict.keys())[index-1]
+    else:
+        median = (list(sorted_dict.keys())[index-1] + list(sorted_dict.keys())[index]) / 2
+    return(median)
+
+def find_mean(sorted_dict):
+    """
+    finds the mean of a dictionary with format {value:occurences}
+    
+    inputs
+    ------
+    sorted_dict : a dictionary sorted by its keys
+    
+    outputs
+    -------
+    mean : the mean of the dataset
+    
+    example dictionary:
+    {3:2, 4:5, 6:3} = [3,3,4,4,4,4,4,6,6,6]
+    """
+    total = 0
+    for key, value in sorted_dict.items():
+        total += key*value
+    mean = total / sum(sorted_dict.values())
+    return(mean)
+def get_triangles_per_vertex(my_core, all_meshset):
+    """
+    gets stats for the number of trianges touching each vertex in a file
+    inputs
+    ------
+    my_core : a MOAB core instance
+    all_meshset : a meshset containing all mesh entities in a file
+    
+    outputs
+    -------
+    triangles_per_vertex_stats : a dictionary with certain statistics
+    """
+    tri_meshset = my_core.create_meshset() #creates a meshset
+    tri_range = my_core.get_entities_by_type(all_meshset, types.MBTRI) #retrieves all entities with type triangle
+    my_core.add_entities(tri_meshset, tri_range) #adds the entities into the new meshset
+    all_verts = [] 
+    freq_list = []
+    for tri in range(tri_range.size()): #range should be the length of the tri_range, working on scalability, so may not be
+        connect = my_core.get_adjacencies(tri_range[tri], 0) #stores the vertices of each triangle
+        for vert in range(3):
+            all_verts.append(connect[vert]) #adds each individual vertex into a list
+    sorted_dict = make_verts_dictionary(all_verts)
+    #makes a dictionary of all statistics
+    triangles_per_vertex_stats = {}
+    triangles_per_vertex_stats['minimum'] = min(sorted_dict.keys())
+    triangles_per_vertex_stats['maximum'] = max(sorted_dict.keys())
+    median = find_median(sorted_dict)
+    triangles_per_vertex_stats['median'] = median
+    mean = find_mean(sorted_dict)
+    triangles_per_vertex_stats['average'] = mean
+    return triangles_per_vertex_stats
--- generate_stats.py
+++ generate_stats.py
@@ -5,11 +5,17 @@ import sys
 sys.path.append('/opt/tljh/user/lib/moab/lib/python3.6/site-packages/pymoab-5.1.0-py3.6-linux-x86_64.egg')
 from pymoab import core, types
 from pymoab.rng import Range
-
+import matplotlib.pyplot as plt
+import numpy as np
 # import the new module that defines each of the functions
 import dagmc_stats
+import argparse
 
-def report_stats(entity_ranges):
+
+
+
+
+def report_stats(entity_ranges, surface_per_volume_stats, triangle_per_vertex_stats, verbose):
     """
     Method to print a table of statistics.
     
@@ -17,28 +23,46 @@ def report_stats(entity_ranges):
     ------
     entity_ranges : a dictionary with one entry for each entity type that is a Range of handles to that type
     """
-
-    for entity_type, eh_range in entity_ranges.items():
-        print("There are {} entities of type {}.".format(eh_range.size(),entity_type))
+    if verbose: #if the user wants verbosity, print with more words
+        for entity_type, eh_range in entity_ranges.items():
+            print("There are {} entities of type {}.".format(eh_range.size(),entity_type))
+        print('Type 0: Vertices \nType 2: Triangles \nType 11: EntitySets')
+        for stat, number in surface_per_volume_stats.items():
+            print('The {} number of Surfaces per Volume in this file is {}'.format(stat, number))
+        for stat, number in triangle_per_vertex_stats.items():
+            print('The {} number of Triangles per Vertex in this file is {}'.format(stat, number))
+    else: #or, print with minimal words
+        for entity_type, eh_range in entity_ranges.items():
+            print("Type {}: {}".format(entity_type, eh_range.size()))
+        print("Surfaces per Volume:")
+        for stat, number in surface_per_volume_stats.items():
+            print('{}:{}'.format(stat, number))
+        print("Triangles per Vertex:")
+        for stat, number in triangle_per_vertex_stats.items():
+            print('{}:{}'.format(stat, number))
 
 def main():
 
-    # starting with a single input file - will need to convert this to a user option
-    input_file = "3vols.h5m"
+    # allows the user to input the file name into the command line
+    parser = argparse.ArgumentParser() 
+    parser.add_argument("filename", help = "the file that you want read")
+    parser.add_argument("-v", "--verbose", action = "store_true", help = "increase output verbosity") #optional verbosity setting
+    args = parser.parse_args() 
+    input_file = args.filename
+    verbose = args.verbose
+    #input_file = "3vols.h5m"
 
     my_core = core.Core() #initiates core
     all_meshset = my_core.create_meshset() #creates meshset
-    my_core.load_file(input_file, all_meshset) #dumps all entities into the meshset to be redistributed to other meshsets
-
+    my_core.load_file(input_file) #loads the file
+    all_meshset = my_core.get_root_set() #dumps all entities into the meshset to be redistributed to other meshsets
     # get tags
     dagmc_tags = dagmc_stats.get_dagmc_tags(my_core)
-    
-    # get Ranges of various entities
-    entity_types = [types.MBVERTEX, types.MBTRI]
-    entity_ranges = dagmc_stats.get_entity_ranges(my_core, all_meshset, entity_types)
-
-    report_stats(entity_ranges)
-    
+    entity_types = [types.MBVERTEX, types.MBTRI, types.MBENTITYSET]
+    entity_ranges = dagmc_stats.get_entity_ranges(my_core, all_meshset, entity_types, dagmc_tags)     # get Ranges of various entities
+    surface_per_volume_stats = dagmc_stats.get_surfaces_per_volume(my_core, entity_ranges) #get stats for surface per volume
+    triangle_per_vertex_stats = dagmc_stats.get_triangles_per_vertex(my_core, all_meshset) #get stats for triangle per vertex
+    report_stats(entity_ranges, surface_per_volume_stats, triangle_per_vertex_stats, verbose) #report the statistics
+    #show_histograms()
 if __name__ == "__main__":
     main()
-    
