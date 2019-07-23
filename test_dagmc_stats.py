280a007dccf6c8da87823d104d9888c6df0d1a06
Eliot Pickhardt <eliotpickhar@gmail.com>
2019-07-23 21:12:44 +0000

    Added tests for the ranges

--- test_dagmc_stats.py
+++ test_dagmc_stats.py
@@ -7,72 +7,36 @@ from pymoab.rng import Range
 
 # import the new module that defines each of the functions
 import dagmc_stats
-from collections import OrderedDict
-import statistics
 import nose
-import random as rd
 test_input = "3vols.h5m"
 
 my_core = core.Core()
 my_core.load_file(test_input)
-test_set = {}
-for x in range(rd.randint(1,10)):
-    test_set[rd.randint(0,100)] = rd.randint(1,10000)
+root_set = my_core.get_root_set()
+entity_types = [types.MBVERTEX, types.MBTRI, types.MBENTITYSET]
 def test_get_tags():
 
     dagmc_tags = dagmc_stats.get_dagmc_tags(my_core)
     assert(len(dagmc_tags) == 3)
 
-def test_get_median():
-    '''
-    compares my function with one found online and one that converts it to a list to find the median. 
-    '''
-    # Sort the dictionary
-    values_sorted = OrderedDict(sorted(test_set.items(), key=lambda t: t[0]))
-    index = sum(values_sorted.values())/2
-
-    # Decide whether the number of records is an even or odd number
-    if (index).is_integer():
-        even = True
-    else: 
-        even = False
-
-    x = True
-
-    # Compute median
-    for value, occurences in values_sorted.items():
-        index -= occurences
-        if index < 0 and x is True:
-            median_manual = value
-            break
-        elif index == 0 and even is True:
-            median_manual = value/2
-            x = False
-        elif index < 0 and x is False:
-
-            median_manual += value/2
-            break
-
-    # Create a list of all records and compute median using statistics package
-    values_list = list()
-    for val, count in test_set.items():
-        for count in range(count):
-            values_list.append(val)
-
-    median_computed = statistics.median(values_list)
-    median = dagmc_stats.find_median(test_set)
-    assert(median == median_manual == median_computed)
     
-def test_median():
-    my_mean = dagmc_stats.find_mean(test_set)
-    values_list = list()
-    for val, count in test_set.items():
-        for count in range(count):
-            values_list.append(val)
-    computed_mean = statistics.mean(values_list)
-    assert(my_mean == computed_mean)
+def test_get_native_ranges():
+    native_ranges = dagmc_stats.get_native_ranges(my_core, root_set, entity_types)
+    vertex_range = my_core.get_entities_by_type(root_set, types.MBVERTEX)
+    triangle_range = my_core.get_entities_by_type(root_set, types.MBTRI)
+    entityset_range = my_core.get_entities_by_type(root_set, types.MBENTITYSET)
+    assert(vertex_range == native_ranges[0], triangle_range == native_ranges[2], entityset_range == native_ranges[11])
 
     
+def test_get_entityset_ranges():
+    dagmc_tags = dagmc_stats.get_dagmc_tags(my_core)
+    entityset_ranges = dagmc_stats.get_entityset_ranges(my_core, root_set, dagmc_tags['geom_dim'])
+    node_range = my_core.get_entities_by_type_and_tag(root_set, types.MBENTITYSET, dagmc_tags['geom_dim'], [0])
+    curve_range = my_core.get_entities_by_type_and_tag(root_set, types.MBENTITYSET, dagmc_tags['geom_dim'], [1])
+    surface_range = my_core.get_entities_by_type_and_tag(root_set, types.MBENTITYSET, dagmc_tags['geom_dim'], [2])
+    volume_range = my_core.get_entities_by_type_and_tag(root_set, types.MBENTITYSET, dagmc_tags['geom_dim'], [3])
+    assert(node_range == entityset_ranges['Nodes'], curve_range == entityset_ranges['Curves'], surface_range ==
+           entityset_ranges['Surfaces'], volume_range == entityset_ranges['Volumes'])
     
     
     
