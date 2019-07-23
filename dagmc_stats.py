f011f1a101ed89f02f1da6de299dbd0344274d4d
Eliot Pickhardt <eliotpickhar@gmail.com>
2019-07-23 22:24:12 +0000

    Ran autopep 8

--- dagmc_stats.py
+++ dagmc_stats.py
@@ -3,12 +3,14 @@ import sys
 sys.path.append('/opt/tljh/user/lib/moab/lib/python3.6/site-packages/pymoab-5.1.0-py3.6-linux-x86_64.egg')
 from pymoab import core, types
 from pymoab.rng import Range
+import numpy as np
+
 
 
 def get_dagmc_tags(my_core):
     """
     Get a dictionary with the important tags for DAGMC geometries
-    
+
     inputs
     ------
     my_core : a MOAB Core instance
@@ -18,14 +20,14 @@ def get_dagmc_tags(my_core):
     """
 
     dagmc_tags = {}
-    dagmc_tags['geom_dim'] = my_core.tag_get_handle('GEOM_DIMENSION', size = 1, tag_type = types.MB_TYPE_INTEGER, #creates tag for the geo-
-                                                 storage_type = types.MB_TAG_SPARSE, create_if_missing = True)# geometric dimension
-    
-    dagmc_tags['category'] = my_core.tag_get_handle('CATEGORY', size = 32, tag_type = types.MB_TYPE_OPAQUE, #creates tag for the word of 
-                                                 storage_type = types.MB_TAG_SPARSE, create_if_missing = True) #the category
-    
-    dagmc_tags['global_id'] = my_core.tag_get_handle('GLOBAL_ID', size = 1, tag_type = types.MB_TYPE_INTEGER, #creates tag for each entity
-                                                  storage_type = types.MB_TAG_SPARSE, create_if_missing = True) #id
+    dagmc_tags['geom_dim'] = my_core.tag_get_handle('GEOM_DIMENSION', size=1, tag_type=types.MB_TYPE_INTEGER,  # creates tag for the geo-
+                                                    storage_type=types.MB_TAG_SPARSE, create_if_missing=True)  # geometric dimension
+
+    dagmc_tags['category'] = my_core.tag_get_handle('CATEGORY', size=32, tag_type=types.MB_TYPE_OPAQUE,  # creates tag for the word of
+                                                    storage_type=types.MB_TAG_SPARSE, create_if_missing=True)  # the category
+
+    dagmc_tags['global_id'] = my_core.tag_get_handle('GLOBAL_ID', size=1, tag_type=types.MB_TYPE_INTEGER,  # creates tag for each entity
+                                                     storage_type=types.MB_TAG_SPARSE, create_if_missing=True)  # id
 
     return dagmc_tags
 
@@ -33,33 +35,35 @@ def get_dagmc_tags(my_core):
 def get_native_ranges(my_core, meshset, entity_types):
     """
     Get a dictionary with MOAB ranges for each of the requested entity types
-    
+
     inputs
     ------
     my_core : a MOAB Core instance
     meshset : a MOAB meshset to query for the ranges of entities
     entity_types : a list of valid pyMOAB types to be retrieved
-    
+
     outputs
     -------
     native_ranges : a dictionary with one entry for each entity type that is a Range of handles to that type
     """
-    
+
     native_ranges = {}
     for entity_type in entity_types:
-        native_ranges[entity_type] = my_core.get_entities_by_type(meshset, entity_type) 
+        native_ranges[entity_type] = my_core.get_entities_by_type(
+            meshset, entity_type)
     return native_ranges
+
 
 def get_entityset_ranges(my_core, meshset, geom_dim):
     """
     Get a dictionary with MOAB Ranges that are specific to the types.MBENTITYSET type
-    
+
     inputs
     ------
     my_core : a MOAB Core instance
     meshset : the root meshset for the file
     geom_dim : the tag that specifically denotes the dimesion of the entity
-    
+
     outputs
     -------
     entityset_ranges : a dictionary with one entry for each entityset type, and the value is the range of entities that corrospond to 
@@ -77,20 +81,19 @@ def get_entityset_ranges(my_core, meshset, geom_dim):
 def get_surfaces_per_volume(my_core, entityset_ranges):
     """
     Get the number of surfaces that each volume in a given file contains
-    
+
     inputs
     ------
     my_core : a MOAB core instance
     entity_ranges : a dictionary of the entityset ranges of each tag in a file
-    
+
     outputs
     -------
     freqs : a list of the number of surfaces each volume in the file contains
-    
+
     """
 
     freqs = []
     for volumeset in entityset_ranges['Volumes']:
         freqs.append(my_core.get_child_meshsets(volumeset).size())
     return freqs
-
