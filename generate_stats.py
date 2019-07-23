e3d46616849fa61cac98b93f9f23640ad2f26c25
Eliot Pickhardt <eliotpickhar@gmail.com>
2019-07-23 20:58:55 +0000

    Changed the format of the stats

--- generate_stats.py
+++ generate_stats.py
@@ -5,8 +5,6 @@ import sys
 sys.path.append('/opt/tljh/user/lib/moab/lib/python3.6/site-packages/pymoab-5.1.0-py3.6-linux-x86_64.egg')
 from pymoab import core, types
 from pymoab.rng import Range
-import matplotlib.pyplot as plt
-import numpy as np
 # import the new module that defines each of the functions
 import dagmc_stats
 import argparse
@@ -15,41 +13,38 @@ import argparse
 
 
 
-def report_stats(entity_ranges, surface_per_volume_stats, triangle_per_vertex_stats, triangle_per_surface_stats, verbose):
+def report_stats(stats, verbose):
     """
     Method to print a table of statistics.
     
     inputs
     ------
-    entity_ranges : a dictionary with one entry for each entity type that is a Range of handles to that type
+    stats : a dictionary with information about certain statistics for a model
     """
-    type_legend = {0:'Vertex', 2:'Triangle', 11: 'EntitySet'}
+    
     if verbose: #if the user wants verbosity, print with more words
-        for entity_type, eh_range in entity_ranges.items():
-            if type(entity_type) == int:
-                entity_type = type_legend[entity_type]
-            print("There are {} entities of type {}.".format(eh_range.size(),entity_type))
-        for stat, number in surface_per_volume_stats.items():
-            print('The {} number of Surfaces per Volume in this file is {}'.format(stat, number))
-        for stat, number in triangle_per_vertex_stats.items():
-            print('The {} number of Triangles per Vertex in this file is {}'.format(stat, number))
-        for stat, number in triangle_per_surface_stats.items():
-            print('The {} number of Triangles per Surface in this file is {}'.format(stat, number))
+        for native_type, native_range in stats['Native Ranges'].items():
+            print("There are {} entities of type {}.".format(native_range.size(), native_type))
+        for entityset_type, entityset_range in stats['EntitySet Ranges'].items():
+            print("There are {} entities of type {}.".format(entityset_range.size(), entityset_type))
     else: #or, print with minimal words
-        for entity_type, eh_range in entity_ranges.items():
-            if type(entity_type) == int:
-                entity_type = type_legend[entity_type]
-            print("Type {}: {}".format(entity_type, eh_range.size()))
-        print("Surfaces per Volume:")
-        for stat, number in surface_per_volume_stats.items():
-            print('{}:{}'.format(stat, number))
-        print("Triangles per Vertex:")
-        for stat, number in triangle_per_vertex_stats.items():
-            print('{}:{}'.format(stat, number))
-        print('Triangles per Surface:')
-        for stat, number in triangle_per_surface_stats.items():
-            print('{}:{}'.format(stat, number))
+        for native_type, native_range in stats['Native Ranges'].items():
+            print("Type {}: {}".format(native_type, native_range.size()))
+        for entityset_type, entityset_range in stats['EntitySet Ranges'].items():
+            print("Type {}: {}".format(entityset_type, entityset_range.size()))
+        
+            
 
+def collect_statistics(my_core, root_set):
+    stats = {}
+    dagmc_tags = dagmc_stats.get_dagmc_tags(my_core)
+    entity_types = [types.MBVERTEX, types.MBTRI, types.MBENTITYSET]
+    native_ranges = dagmc_stats.get_native_ranges(my_core, root_set, entity_types)     # get Ranges of various entities
+    entityset_ranges = dagmc_stats.get_entityset_ranges(my_core, root_set, dagmc_tags['geom_dim'])
+    stats['Native Ranges'] = native_ranges
+    stats['EntitySet Ranges'] = entityset_ranges
+    return stats
+    
 def main():
 
     # allows the user to input the file name into the command line
@@ -62,19 +57,11 @@ def main():
     #input_file = "3vols.h5m"
 
     my_core = core.Core() #initiates core
-    all_meshset = my_core.create_meshset() #creates meshset
     my_core.load_file(input_file) #loads the file
-    all_meshset = my_core.get_root_set() #dumps all entities into the meshset to be redistributed to other meshsets
-    # get tags
-    dagmc_tags = dagmc_stats.get_dagmc_tags(my_core)
-    entity_types = [types.MBVERTEX, types.MBTRI, types.MBENTITYSET]
-    native_ranges = dagmc_stats.get_native_ranges(my_core, all_meshset, entity_types)     # get Ranges of various entities
-    entity_ranges = dagmc_stats.get_entityset_ranges(my_core, all_meshset, native_ranges, dagmc_tags)
-    surface_per_volume_stats = dagmc_stats.get_surfaces_per_volume(my_core, entity_ranges) #get stats for surface per volume
-    triangle_per_vertex_stats = dagmc_stats.get_triangles_per_vertex(my_core, entity_ranges) #get stats for triangle per vertex
-    triangle_per_surface_stats = dagmc_stats.get_triangles_per_surface(my_core, entity_ranges) #get stats for triangle per surface
-    report_stats(entity_ranges, surface_per_volume_stats, triangle_per_vertex_stats, triangle_per_surface_stats, 
-                 verbose) #report the statistics
-    #show_histograms()
+    root_set = my_core.get_root_set() #dumps all entities into the meshset to be redistributed to other meshsets
+    stats = collect_statistics(my_core, root_set)
+    report_stats(stats, verbose)
+    
+    
 if __name__ == "__main__":
     main()
