bdc73b9359cefa1b7f582e2473bbf6bea217aad1
Eliot Pickhardt <eliotpickhar@gmail.com>
2019-07-24 19:04:12 +0000

    reports t_p_s stats

--- generate_stats.py
+++ generate_stats.py
@@ -2,13 +2,13 @@
 
 # set the path to find the current installation of pyMOAB
 import sys
-import argparse
 sys.path.append('/opt/tljh/user/lib/moab/lib/python3.6/site-packages/pymoab-5.1.0-py3.6-linux-x86_64.egg')
 from pymoab import core, types
 from pymoab.rng import Range
 # import the new module that defines each of the functions
 import dagmc_stats
-
+import argparse
+import numpy as np
 
 
 
@@ -24,17 +24,32 @@ def report_stats(stats, verbose):
     """
     
     if verbose: #if the user wants verbosity, print with more words
-        for native_type, native_range in stats['Native Ranges'].items():
-            print("There are {} entities of type {}.".format(native_range.size(), native_type))
-        for entityset_type, entityset_range in stats['EntitySet Ranges'].items():
-            print("There are {} entities of type {}.".format(entityset_range.size(), entityset_type))
+        for statistic, value in stats['T_P_S'].items():
+            print("The {} number of Triangles per Surface in this model is {}.".format(value, statistic))
     else: #or, print with minimal words
-        for native_type, native_range in stats['Native Ranges'].items():
-            print("Type {}: {}".format(native_type, native_range.size()))
-        for entityset_type, entityset_range in stats['EntitySet Ranges'].items():
-            print("Type {}: {}".format(entityset_type, entityset_range.size()))
+        print("Triangles per Surface:")
+        for statistic, value in stats['T_P_S'].items():
+            print("{} : {}".format(statistic, value))
         
-            
+def get_stats(data):
+    """
+    gets the minimum, maximum, median, and mean for a dataset
+    
+    inputs
+    ------
+    data : a dataset in list form
+    
+    outputs
+    -------
+    statistics : a dictionary of statistics for a given dataset
+    
+    """
+    statistics = {}
+    statistics['minimum'] = min(data)
+    statistics['maximum'] = max(data)
+    statistics['median'] = np.median(data)
+    statistics['mean'] = np.mean(data)
+    return statistics
 
 def collect_statistics(my_core, root_set):
     """
@@ -53,10 +68,12 @@ def collect_statistics(my_core, root_set):
     stats = {}
     dagmc_tags = dagmc_stats.get_dagmc_tags(my_core)
     entity_types = [types.MBVERTEX, types.MBTRI, types.MBENTITYSET]
-    stats['Native Ranges'] = dagmc_stats.get_native_ranges(my_core, root_set, entity_types)     # get Ranges of various entities
-    stats['EntitySet Ranges'] = dagmc_stats.get_entityset_ranges(my_core, root_set, dagmc_tags['geom_dim']) 
+    native_ranges = dagmc_stats.get_native_ranges(my_core, root_set, entity_types)     # get Ranges of various entities
+    entityset_ranges = dagmc_stats.get_entityset_ranges(my_core, root_set, dagmc_tags['geom_dim'])
+    t_p_s_data = dagmc_stats.get_triangles_per_surface(my_core, entityset_ranges)
+    t_p_s_stats = get_stats(t_p_s_data)
+    stats['T_P_S'] = t_p_s_stats
     return stats
-    
     
 def main():
 
@@ -67,7 +84,7 @@ def main():
     args = parser.parse_args() 
     input_file = args.filename
     verbose = args.verbose
-
+    #input_file = "3vols.h5m"
 
     my_core = core.Core() #initiates core
     my_core.load_file(input_file) #loads the file
@@ -77,5 +94,4 @@ def main():
     
     
 if __name__ == "__main__":
-    main()
-    
+    main() 
