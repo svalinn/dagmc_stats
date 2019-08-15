7850d7639c33404f634cafadba7e4fbff1bb9df8
Eliot Pickhardt <eliotpickhar@gmail.com>
2019-08-15 15:58:01 +0000

    further the aspect ratio

--- dagmc_stats.py
+++ dagmc_stats.py
@@ -1,24 +1,28 @@
 # set the path to find the current installation of pyMOAB
 import sys
 import numpy as np
+import math
 sys.path.append(
     '/opt/tljh/user/lib/moab/lib/python3.6/site-packages/pymoab-5.1.0-py3.6-linux-x86_64.egg')
 from pymoab.rng import Range
 from pymoab import core, types
 
+
 def get_dagmc_tags(my_core):
     """
     Get a dictionary with the important tags for DAGMC geometries
-
+    
     inputs
     ------
     my_core : a MOAB Core instance
+    
     outputs
     -------
     dagmc_tags : a dictionary of relevant tags
     """
 
     dagmc_tags = {}
+
     dagmc_tags['geom_dim'] = my_core.tag_get_handle('GEOM_DIMENSION', size=1, tag_type=types.MB_TYPE_INTEGER,   
                                                     storage_type=types.MB_TAG_SPARSE, create_if_missing=True)  # geometric dimension
 
@@ -26,6 +30,7 @@ def get_dagmc_tags(my_core):
                                                     storage_type=types.MB_TAG_SPARSE, create_if_missing=True)  # the category
 
     dagmc_tags['global_id'] = my_core.tag_get_handle('GLOBAL_ID', size=1, tag_type=types.MB_TYPE_INTEGER,  
+
                                                      storage_type=types.MB_TAG_SPARSE, create_if_missing=True)  # id
 
     return dagmc_tags
@@ -34,16 +39,17 @@ def get_dagmc_tags(my_core):
 def get_native_ranges(my_core, meshset, entity_types):
     """
     Get a dictionary with MOAB ranges for each of the requested entity types
-
+    
     inputs
     ------
     my_core : a MOAB Core instance
     meshset : a MOAB meshset to query for the ranges of entities
     entity_types : a list of valid pyMOAB types to be retrieved
-
+    
     outputs
     -------
-    native_ranges : a dictionary with one entry for each entity type that is a Range of handles to that type
+    native_ranges : a dictionary with one entry for each entity type that is a
+                    Range of handles to that type
     """
 
     native_ranges = {}
@@ -55,29 +61,100 @@ def get_native_ranges(my_core, meshset, entity_types):
 
 def get_entityset_ranges(my_core, meshset, geom_dim):
     """
-    Get a dictionary with MOAB Ranges that are specific to the types.MBENTITYSET type
-
+    Get a dictionary with MOAB Ranges that are specific to the types.MBENTITYSET
+    type
+    
     inputs
     ------
     my_core : a MOAB Core instance
     meshset : the root meshset for the file
     geom_dim : the tag that specifically denotes the dimesion of the entity
-
+    
     outputs
     -------
-    entityset_ranges : a dictionary with one entry for each entityset type, and the value is the range of entities that corrospond to 
-                        each type
-
+    entityset_ranges : a dictionary with one entry for each entityset type, 
+                       and the value is the range of entities that corrospond to each
+                       type
     """
+    
     entityset_ranges = {}
     entityset_types = ['Nodes', 'Curves', 'Surfaces', 'Volumes']
     for dimension, set_type in enumerate(entityset_types):
-        entityset_ranges[set_type] = my_core.get_entities_by_type_and_tag(meshset, types.MBENTITYSET, geom_dim, 
+        entityset_ranges[set_type] = my_core.get_entities_by_type_and_tag(meshset, types.MBENTITYSET, geom_dim,
                                                                           [dimension])
     return entityset_ranges
 
 
-def get_triangle_aspect_ratio(my_core, meshset, entityset_ranges):
+def get_triangles_per_vertex(my_core, native_ranges):
+    """
+    This function will return data about the number of triangles on each
+    vertex in a file
+    
+    inputs
+    ------
+    my_core : a MOAB Core instance
+    native_ranges : a dictionary containing ranges for each native type in the file (VERTEX, TRIANGLE, ENTITYSET)
+    
+    outputs
+    -------
+    t_p_v_data : a list of the number of triangles each vertex touches
+    """
+    
+    t_p_v_data = []
+    tri_dimension = 2
+    for vertex in native_ranges[0]:
+        t_p_v_data.append(my_core.get_adjacencies(vertex, tri_dimension).size())
+    return t_p_v_data
+  
+  
+def get_triangles_per_surface(my_core, entity_ranges):
+    """
+    This function will return data about the number of triangles on each
+    vertex in a file
+    
+    inputs
+    ------
+    my_core : a MOAB Core instance
+    entity_ranges : a dictionary containing ranges for each type in the file 
+                    (VOLUME, SURFACE, CURVE, VERTEX, TRIANGLE, ENTITYSET)
+    
+    outputs
+    -------
+    t_p_s : a dictionary containing the entityhandle of the surface,
+            and the number of triangles each surface contains.
+            i.e {surface entityhandle : triangles it contains}
+    """
+
+    t_p_s = {}
+    for surface in entity_ranges['Surfaces']:
+        t_p_s[surface] = my_core.get_entities_by_type(
+                                 surface, types.MBTRI).size()
+    return t_p_s
+
+  
+def get_surfaces_per_volume(my_core, entityset_ranges):
+    """
+    Get the number of surfaces that each volume in a given file contains
+    
+    inputs
+    ------
+    my_core : a MOAB core instance
+    entity_ranges : a dictionary of the entityset ranges of each tag in a file
+    
+    outputs
+    -------
+    s_p_v : a dictionary containing the volume entityhandle
+            and the number of surfaces each volume in the file contains
+            i.e. {volume entityhandle:surfaces it contains}
+    """
+
+    s_p_v = {}
+    for volumeset in entityset_ranges['Volumes']:
+        s_p_v[volumeset] = my_core.get_child_meshsets(volumeset).size()
+    return s_p_v
+
+
+def get_triangle_aspect_ratio(my_core, meshset):
     """
     Gets the triangle aspect ratio (according to the equation: (abc)/(8(s-a)(s-b)(s-c)), where s = .5(a+b+c).)
     
@@ -92,42 +169,38 @@ def get_triangle_aspect_ratio(my_core, meshset, entityset_ranges):
     -------
     t_a_r : (list) the triangle aspect ratios for all triangles in the meshset
     """
-
     
     t_a_r = []
-    tris = core.get_entities_by_type(entity_ranges['Surfaces'][0], types.MBTRI)
+    indexes = [1,2,0]
+    
+    tris = my_core.get_entities_by_type(meshset, types.MBTRI)
+    
     for triangle in tris:
         side_lengths = []
         s = 0
-        verts = list(core.get_adjacencies(triangle, 0))
         coord_list = []
+        
+        verts = list(my_core.get_adjacencies(triangle, 0))
+        
         for side in range(3):
-            coords = list(core.get_coords(verts[side]))
+            coords = list(my_core.get_coords(verts[side]))
             coord_list.append(coords)
+            
         coord_array = np.array(coord_list)
+        
         for length in range(3):    
             sum_squares = 0
+            
             for vert in range(3):
-                sum_squares += coord_array[vert,length]**2
+                sum_squares += (coord_array[length, vert] - coord_array[indexes[length], vert])**2
+            
             side_lengths.append(math.sqrt(sum_squares))
-            s += .5*(side_lengths[length])
+        
+        s = .5*(sum(side_lengths))
         top = np.prod(side_lengths)
         bottom = 8*(s-side_lengths[0])*(s-side_lengths[1])*(s-side_lengths[2])
-        print(top, bottom, s)
+        
         t_a_r.append(top/bottom)
-    print(t_a_r)
+    
+    return t_a_r
             
-            
-        
-        
-
-
-
-
-
-
-
-
-
-
-
