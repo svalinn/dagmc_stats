a5a6b0db03a16639c3118bfa45cb24960995f5ea
Yuehan Qin <yqin43@wisc.edu>
2021-03-09 22:55:16 +0800

    updates for test file

--- tests/test_ds.py
+++ tests/test_ds.py
@@ -1,6 +1,7 @@
 from pymoab import core, types
 from pymoab.rng import Range
 import dagmc_stats.DagmcStats as ds
+import pandas as pd
 
 test_env = [{'input_file': 'tests/3vols.h5m'}, {
     'input_file': 'tests/single-cube.h5m'}, {'input_file': 'tests/pyramid.h5m'}]
@@ -14,17 +15,29 @@ def test_load_file():
 
     print(single_cube.root_set)
     assert(single_cube.root_set == 0)
-
-    print(single_cube._vert_data)
-    print(single_cube._tri_data)
-    print(single_cube._surf_data)
-    print(single_cube._vol_data)
-    pass
+    
+    # tests for pandas data frames
+    exp_vert_data = pd.DataFrame(
+            columns=['vert_eh', 'roughness', 'tri_per_vert'])
+    exp_vert_data = exp_vert_data.set_index('vert_eh')
+    assert(single_cube._vert_data.equals(exp_vert_data))
+    
+    exp_tri_data = pd.DataFrame(
+            columns=['tri_eh', 'aspect_ratio', 'area'])
+    exp_tri_data = exp_tri_data.set_index('tri_eh')
+    assert(single_cube._tri_data.equals(exp_tri_data))
+    
+    exp_surf_data = pd.DataFrame(
+            columns=['surf_eh', 'tri_per_surf', 'coarseness'])
+    exp_surf_data = exp_surf_data.set_index('surf_eh')
+    assert(single_cube._surf_data.equals(exp_surf_data))
+    
+    exp_vol_data = pd.DataFrame(
+            columns=['vol_eh', 'surf_per_vol', 'coarseness'])
+    exp_vol_data = exp_vol_data.set_index('vol_eh')
+    assert(single_cube._vol_data.equals(exp_vol_data))
 
 
 def test_load_file_populate():
     # another test to test load_file() with populate=True
     pass
-
-
-test_load_file()
