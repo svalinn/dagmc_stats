8f3410955e163e2b255c9ea4b57c58b08b7979dd
Yuehan Qin <yqin43@wisc.edu>
2021-05-25 20:27:25 +0800

    step 1

--- dagmc_stats/DagmcFile.py
+++ dagmc_stats/DagmcFile.py
@@ -107,42 +107,7 @@ class DagmcStats:
                 self._my_moab_core.get_entities_by_type_and_tag(self.root_set, types.MBENTITYSET,
                                                                 self.dagmc_tags['geom_dim'], [dimension])
 
-    def get_tris(self, meshset=None):
-        """Get triangles of a volume if geom_dim is 3
-        Get triangles of a surface if geom_dim is 2
-        Else get all the triangles
 
-        inputs
-        ------
-        meshset : set of entities that are used to populate data. Default value
-                  is None and data of the whole geometry will be populated.
-
-        outputs
-        -------
-        tris : a list of triangle entities
-        """
-        meshset_lst = []
-        tris_lst = []
-
-        if meshset is None or meshset == self.root_set:
-            meshset_lst.append(self.root_set)
-        else:
-            dim = self._my_moab_core.tag_get_data(self.dagmc_tags['geom_dim'], meshset)[0][0]
-            # get triangles of a volume
-            if dim == 3:
-                surfs = self._my_moab_core.get_child_meshsets(meshset)
-                meshset_lst.extend(surfs)
-            # get triangles of a surface
-            elif dim == 2:
-                meshset_lst.append(meshset)
-            else:
-                warnings.warn('Meshset is not a volume nor a surface! Rootset will be used by default.')
-                meshset_lst.append(self.root_set)
-
-        for item in meshset_lst:
-            tris = self._my_moab_core.get_entities_by_type(item, types.MBTRI)
-            tris_lst.extend(tris)
-        return tris
 
     def __populate_triangle_data(self, meshset):
         """Populate triangle areas and triangle aspect ratios
@@ -176,7 +141,6 @@ class DagmcStats:
 
         inputs
         ------
-        my_core : a MOAB Core instance
         tri : triangle entity
 
         outputs
