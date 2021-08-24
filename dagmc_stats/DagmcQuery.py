c2f20ee1f2223fe9bde17a8a3781e00df685e7e6
Yuehan Qin <yqin43@wisc.edu>
2021-08-24 21:22:54 +0800

    get_entities

--- dagmc_stats/DagmcQuery.py
+++ dagmc_stats/DagmcQuery.py
@@ -29,23 +29,8 @@ class DagmcQuery:
         self._surf_data = pd.DataFrame()
         self._vol_data = pd.DataFrame()
 
-    def get_tris(self):
-        """Get triangles of a volume if geom_dim is 3
-        Get triangles of a surface if geom_dim is 2
-        Else get all the triangles
-
-        inputs
-        ------
-        meshset : set of entities that are used to populate data. Default value
-                  is None and data of the whole geometry will be populated.
-
-        outputs
-        -------
-        tris : a list of triangle entities
-        """
+    def __get_entities(self):
         meshset_lst = []
-        tris_lst = []
-
         if self.meshset is None or self.meshset == self.dagmc_file.root_set:
             meshset_lst.append(self.dagmc_file.root_set)
         else:
@@ -63,7 +48,24 @@ class DagmcQuery:
                 warnings.warn(
                     'Meshset is not a volume nor a surface! Rootset will be used by default.')
                 meshset_lst.append(self.dagmc_file.root_set)
+        return meshset_lst
+                
+    def get_tris(self):
+        """Get triangles of a volume if geom_dim is 3
+        Get triangles of a surface if geom_dim is 2
+        Else get all the triangles
 
+        inputs
+        ------
+        meshset : set of entities that are used to populate data. Default value
+                  is None and data of the whole geometry will be populated.
+
+        outputs
+        -------
+        tris : a list of triangle entities
+        """
+        meshset_lst = self.__get_entities()
+        tris_lst = []
         for item in meshset_lst:
             tris = self.dagmc_file._my_moab_core.get_entities_by_type(
                 item, types.MBTRI)
@@ -82,25 +84,10 @@ class DagmcQuery:
         -------
         none
         """
+        meshset_lst = self.__get_entities()
         t_p_v_data = []
         tri_dimension = 2
-        meshset_lst = []
         verts = []
-        if self.meshset is None or self.meshset == self.dagmc_file.root_set:
-            meshset_lst.append(self.dagmc_file.root_set)
-        else:
-            dim = self.dagmc_file._my_moab_core.tag_get_data(
-                self.dagmc_file.dagmc_tags['geom_dim'], self.meshset)[0][0]
-            if dim == 3:
-                surfs = self.dagmc_file._my_moab_core.get_child_meshsets(
-                    self.meshset)
-                meshset_lst.extend(surfs)
-            elif dim == 2:
-                meshset_lst.append(self.meshset)
-            else:
-                warnings.warn(
-                    'Meshset is not a volume nor a surface! Rootset will be used by default.')
-                meshset_lst.append(self.dagmc_file.root_set)
         for item in meshset_lst:
             verts.extend(self.dagmc_file._my_moab_core.get_entities_by_type(
                 item, types.MBVERTEX))
